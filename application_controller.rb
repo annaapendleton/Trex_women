353dfead77873d5a2f2a60cc9436adb23d53b65d
Anna Pendleton <18apendleton@nerinxhs.org>
2016-06-30 20:53:23 +0000

    ruby changes and other changes we need css

--- application_controller.rb
+++ application_controller.rb
@@ -1,7 +1,30 @@
 require 'bundler'
 Bundler.require
-require_relative 'models/pig_latinize.rb'
+require_relative 'models/model.rb'
 
 class MyApp < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+  get '/questions' do
+    erb :questions
+  end
+
+  get '/results' do
+    erb :results
+  end
+
+post '/result' do
+  i=params[:interest].to_sym
+  l=params[:learn].to_sym
+  b=params[:business].to_sym
+  @name_of_woman = answer(i,l,b)[0]
+  @bio = answer(i,l,b)[1]
+  @pic = answer(i,l,b)[2]
+  erb :results
+end
+
+
 
   end #ends sinatra class
